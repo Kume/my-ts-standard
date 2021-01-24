3d1b96f80b8e2eff3581ea483d29a8612ffaef23
makoto.kume <makoto.kume07@gmail.com>
2021-01-25 03:38:27 +0900

    webからcoreへの参照を追加

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import {testAddPlus1} from '@my-ts-standard/my-ts-core';
 
 export const App: React.FC = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      Hello world
+      <br />
+      Result = {testAddPlus1(2, 8)}
+    </div>
+  );
 };
